32f9f8883215aee56d71b44957eaa8024fc01bd5
Roman Shevyakov <roman@mgstr.ru>
2023-02-09 07:36:57 +0300

    feat: passing  20 of 20 tests

--- src/index.js
+++ src/index.js
@@ -1,58 +1,27 @@
-module.exports = 
-
-
-function check(str, bracketsConfig) {
-  const brktsTypes = {};
+module.exports = function check(str, bracketsConfig) {
   const stack = [];
   let result = true;
+  const brktsTypes = {};
   for (item of bracketsConfig) { //Наполняем объект образцами скобок
-    brktsTypes[item[0]] = item[1];
-  };
+    brktsTypes[item[0]] = item[1]
+  }
 
   for (letter of str) {
-    // console.log(stack);
     for (key in brktsTypes) {
-      if (letter === key && letter !== '|') stack.push(letter);
-      else if (letter === key && letter === '|' && stack.length === 0) stack.push(letter);
-      // else if (letter === '|' && ((stack.length === 0) || (brktsTypes[stack[stack.length - 1]] !== letter))) stack.push(letter);
-      // else if (letter === '|') stack.push(letter);
-      else if (letter === '|' && brktsTypes[stack[stack.length - 1]] !== letter) stack.push(letter);
-      // else if (letter === '|' && brktsTypes[stack[stack.length - 1]] !== letter) stack.push(letter);
+      // Когда добавлять в стэк
+      if (letter === key && key !== brktsTypes[key]) stack.push(letter);
+      else if (letter === key && key === brktsTypes[key] && stack.length === 0) stack.push(letter);
+      else if (letter === key && key === brktsTypes[key] && stack[stack.length - 1] !== letter && stack.length > 0) stack.push(letter);
+      // Когда удалять из стэка или выдавать false
       else if (letter === brktsTypes[key] && ((stack.length === 0) || (stack[stack.length - 1] !== key))) {
         result = false;
         break;
       }
       else if (letter === brktsTypes[key] && stack[stack.length - 1] === key) stack.pop();
-      // else if (letter === brktsTypes[key] && letter === '|') {
-      //   console.log("checking: ", letter);
-      //   console.log('current brackets types: ', brktsTypes);
-      //   console.log('current stack: ', stack);
-      //   stack.pop();
-      // } 
-      // else console.log('Not all cases are written!');
     }
   }
 
   if (stack.length > 0) result = false;
 
-  // console.log(stack, result);
   return result;
 }
-
-// check('|(((|(|)', [['(', ')'], ['|', '|']])               // -> false
-
-
-
-
-// check('|()|(||)||', [['(', ')'], ['|', '|']])         // -> true
-// check('|()|', [['(', ')'], ['|', '|']])               // -> true
-// check('||', [['|', '|']])                             // -> true
-// check('()', [['(', ')']])                             // -> true
-// check('((()))()', [['(', ')']])                       // -> true
-// check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
-// check('[]()', [['(', ')'], ['[', ']']])               // -> true
-// check('[]()(', [['(', ')'], ['[', ']']])              // -> false
-// check('())(', [['(', ')']])                           // -> false
-// check('[(])', [['(', ')'], ['[', ']']])               // -> false
-
-
